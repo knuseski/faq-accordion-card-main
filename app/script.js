13534582ab6d395da9618142ac614e26b820518c
Kristijan Nusheski <Kristijan.Nusheski@netcetera.com>
2021-10-18 10:44:43 +0200

    Done

---
+++ app/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const acc = document.getElementsByClassName("q");
+    console.log(acc)
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+});
+
